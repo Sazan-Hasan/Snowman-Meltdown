6b7fd7c8cbe8f56cabcfe1f927f44fa330f70a16
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-02-18 15:51:55 +0100

    Repeat asking feature is aded

--- game_logic.py
+++ game_logic.py
@@ -5,7 +5,6 @@ WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
 
 def get_random_word():
-    """Select a random word from WORDS."""
     return random.choice(WORDS)
 
 
@@ -23,12 +22,10 @@ def display_game_state(mistakes, secret_word, guessed_letters):
 
 
 def is_word_guessed(secret_word, guessed_letters):
-    """Return True if all letters in secret_word were guessed."""
     return all(letter in guessed_letters for letter in secret_word)
 
 
 def get_valid_guess(guessed_letters):
-    """Ask until user enters a single new alphabetical character."""
     while True:
         guess = input("Guess a letter: ").lower().strip()
 
@@ -42,27 +39,41 @@ def get_valid_guess(guessed_letters):
 
         return guess
 
+def ask_replay():
+    while True:
+        answer = input("Play again? (y/n): ").lower().strip()
+        if answer in ("y", "yes"):
+            return True
+        if answer in ("n", "no"):
+            return False
+        print("Please enter y or n.\n")
 
 def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-    max_mistakes = len(STAGES) - 1
-
     print("Welcome to Snowman Meltdown!")
 
-    while mistakes < max_mistakes and not is_word_guessed(secret_word, guessed_letters):
+    while True:
+
+        secret_word = get_random_word()
+        guessed_letters = []
+        mistakes = 0
+        max_mistakes = len(STAGES) - 1
+
+        while mistakes < max_mistakes and not is_word_guessed(secret_word, guessed_letters):
+            display_game_state(mistakes, secret_word, guessed_letters)
+
+            guess = get_valid_guess(guessed_letters)
+            guessed_letters.append(guess)
+
+            if guess not in secret_word:
+                mistakes += 1
+
         display_game_state(mistakes, secret_word, guessed_letters)
 
-        guess = get_valid_guess(guessed_letters)
-        guessed_letters.append(guess)
+        if is_word_guessed(secret_word, guessed_letters):
+            print("You saved the snowman! The word was:", secret_word)
+        else:
+            print("Game Over! The word was:", secret_word)
 
-        if guess not in secret_word:
-            mistakes += 1
-
-    display_game_state(mistakes, secret_word, guessed_letters)
-
-    if is_word_guessed(secret_word, guessed_letters):
-        print("You saved the snowman! The word was:", secret_word)
-    else:
-        print("Game Over! The word was:", secret_word)
+        if not ask_replay():
+            print("Thanks for playing!")
+            break
